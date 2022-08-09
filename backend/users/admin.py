c9fb22e0562c86596e69bef47cdf52f714911644
Marina <margoloko@mail.ru>
2022-08-09 20:24:06 +0700

    add admin panel for user

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin import register
 
-# Register your models here.
+from .models import User 
+#Follow
+
+
+@register(User)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = (
+        'username', 'first_name', 'last_name', 'email',)
+    exclude = ('password',)
+    list_filter = ('first_name', 'email',)
+    save_on_top = True
