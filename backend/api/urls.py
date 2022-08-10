e74005bfdd9ac039ff5d6b2c830a68c1b385c7a7
Marina <margoloko@mail.ru>
2022-08-10 15:01:03 +0700

    add view Tag and Ingredients

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,2 +1,22 @@
+from django.urls import include, path, re_path
+from rest_framework.routers import DefaultRouter
+
+from .views import IngredientViewSet, TagViewSet 
 
 
+app_name = 'api'
+
+router = DefaultRouter()
+
+#router.register('users', UsersViewSet, basename='users')
+router.register('tags', TagViewSet, basename='tags')
+router.register('ingredients', IngredientViewSet, basename='ingredients')
+#router.register('recipes', RecipeViewSet, basename='recipes')
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    #re_path(r'auth/', include('djoser.urls.authtoken')),
+    #path('', include('djoser.urls')),
+    #path(r'^auth/', include('djoser.urls.authtoken'))
+]
--- backend/api/views.py
+++ backend/api/views.py
@@ -4,11 +4,12 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import decorators, filters, mixins, permissions, status, viewsets
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import AllowAny
 
-from .serializers import IngredientSerializer, RecipeSerializer, TagSerializer
 from .pagination import LimitPagePagination
 from .permissions import AdminOrAuthor, AdminOrReadOnly
 from recipes.models import Ingredient, Recipe, Tag
+from .serializers import IngredientSerializer, RecipeSerializer, TagSerializer
 #from users.models import Follow, User
 
 
@@ -21,13 +22,13 @@ class ListRetrieveViewSet(mixins.ListModelMixin,
 class TagViewSet(ListRetrieveViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
-    permission_classes = (AdminOrReadOnly,)
+    permission_classes = (AllowAny,)
 
 
 class IngredientViewSet(ListRetrieveViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-    permission_classes = (AdminOrReadOnly,)
+    permission_classes = (AllowAny,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('^name',)
 
@@ -35,7 +36,4 @@ class IngredientViewSet(ListRetrieveViewSet):
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
-    #permission_classes = (AdminOrAuthor,)
-    filter_backends = (DjangoFilterBackend,)
-    #filterset_fields = ('is_favorited', 'author', 'is_in_shopping_cart', 'tags',)
     pagination_class = LimitPagePagination
