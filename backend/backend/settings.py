c179120a20985c009b2277d5f6d9b9753f7535cc
Marina <margoloko@mail.ru>
2022-08-15 11:49:43 +0700

    fix authtorization

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -109,12 +109,11 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
-    ],
-
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
     ],
 }
 
