6eebfca6be4887fb4a30cf525d04c35f4516bf2f
Balakhonova Marina <90852680+margoloko@users.noreply.github.com>
2022-08-23 20:14:40 +0700

    Update pagination.py

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -3,5 +3,4 @@ from rest_framework.pagination import PageNumberPagination
 
 class LimitPagePagination(PageNumberPagination):
     page_size = 6
-  #  page_query_param = 'page'
     page_size_query_param = 'limit'
